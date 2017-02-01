653b4d3b4a7a0ea73c89bca997fa5b33bd28de5e
Blair Thompson <mail@justblair.co.uk>
2017-02-01 08:56:06 +0000

    Fixed relative file reference

--- esp8266/sonoffsc/sonoffsc.ino
+++ esp8266/sonoffsc/sonoffsc.ino
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <Arduino.h>
-#include "all.h"
+#include "config\all.h"
 
 // -----------------------------------------------------------------------------
 // PROTOTYPES
