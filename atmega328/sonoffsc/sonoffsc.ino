97a7be1613aa3c5a3548f446fb6df82112999271
Xose Pérez <xose.perez@gmail.com>
2018-01-24 22:35:46 +0100

    Added badges

--- atmega328/sonoffsc/sonoffsc.ino
+++ atmega328/sonoffsc/sonoffsc.ino
@@ -1,7 +1,7 @@
 /*
 
 Sonoff SC
-Copyright (C) 2017 by Xose Pérez <xose dot perez at gmail dot com>
+Copyright (C) 2017-2018 by Xose Pérez <xose dot perez at gmail dot com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
